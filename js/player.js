4127ffdef1fff9a234c4cd1b715d01975d2d4fce
RitaM5 <sporshia108514orva@gmail.com>
2022-11-10 15:57:00 +0600

    list created

--- js/player.js
+++ js/player.js
@@ -5,14 +5,15 @@ function showName(arrayObject) {
     //count++;
     const showData = document.getElementById('show-name');
     //showName.innerText = arrayObject.length;
+    showData.innerHTML="";
     for (let i = 0; i < arrayObject.length; i++) {
         console.log(arrayObject.length);
-        const tr = document.createElement("tr");
-        tr.innerHTML = `
-        <td class="text-white">${i + 1}</td>
-        <td class="text-white ms-2">${arrayObject[i].playerName}</td>`
+        const li = document.createElement("li");
+        li.innerHTML = `
+        <li class="text-white ms-2" style='list-style-type: none;'}>${i+1}. ${arrayObject[i]}</li>`
+        showData.appendChild(li);
     }
-    showData.appendChild(tr);
+  
 }
 function addToCart(element) {
     let playerName = element.parentNode.children[0].innerText;
