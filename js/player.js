c8110ff185943e80bf05df65a7980024e78c38ed
RitaM5 <sporshia108514orva@gmail.com>
2022-11-10 17:22:56 +0600

    calulation part created

--- js/player.js
+++ js/player.js
@@ -2,21 +2,40 @@ console.log('heloo');
 let arrayObject = [];
 let count = 0;
 function showName(arrayObject) {
-    //count++;
     const showData = document.getElementById('show-name');
-    //showName.innerText = arrayObject.length;
-    showData.innerHTML="";
+    showData.innerHTML = "";
     for (let i = 0; i < arrayObject.length; i++) {
         console.log(arrayObject.length);
         const li = document.createElement("li");
         li.innerHTML = `
-        <li class="text-white ms-2" style='list-style-type: none;'}>${i+1}. ${arrayObject[i]}</li>`
+        <li class="text-white ms-2" style='list-style-type: none;'}>${i + 1}. ${arrayObject[i]}</li>`
         showData.appendChild(li);
     }
-  
 }
 function addToCart(element) {
     let playerName = element.parentNode.children[0].innerText;
     arrayObject.push(playerName);
     showName(arrayObject);
+}
+
+function calculate() {
+    let perPlayer = document.getElementById('per-player');
+    let perPrice = parseInt(perPlayer.value);
+    let playerExpenses = document.getElementById('player-expenses');
+    playerExpenses.innerText = perPrice * arrayObject.length;
+
+}
+function calculateTotal() {
+    let perPlayer = document.getElementById('per-player');
+    let perPrice = parseInt(perPlayer.value);
+    let playerExpenses = document.getElementById('player-expenses');
+    let Expenses = playerExpenses.innerText;
+    Expenses = perPrice * arrayObject.length;
+    let manager = document.getElementById('manager');
+    let managerTaka = parseInt(manager.value);
+    let coach = document.getElementById('coach');
+    let coachTaka = parseInt(coach.value);
+    let showTotal = document.getElementById('show-total');
+    showTotal.innerText = parseInt(Expenses + managerTaka + coachTaka);
+
 }
